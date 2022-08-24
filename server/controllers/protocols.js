cec16a4d44a17c4ef2c3471fda6ec0b1786d409a
Xavi Fortuny <fortuny.xavi@gmail.com>
2022-08-24 17:33:12 +0200

    basic cases

--- server/controllers/protocols.js
+++ server/controllers/protocols.js
@@ -1,18 +1,27 @@
 const { avoidMech } = require('../helper_functions/functions');
-const { furthestEnemies } = require('../helper_functions/functions');
+const { prioritizeMech } = require('../helper_functions/functions');
 const { assistAllies } = require('../helper_functions/functions');
 const { avoidCrossfire } = require('../helper_functions/functions');
+const { furthestEnemies } = require('../helper_functions/functions');
 const { closestEnemies } = require('../helper_functions/functions');
-const { prioritizeMech } = require('../helper_functions/functions');
 
 const postProtocol = async function (req, res) {
-	let protocol = req.body.protocols;
 	try {
-		if (protocol[0] === 'avoid-mech') {
-			res.status(200);
-			return await res.send(avoidMech(req.body));
-		} else {
-			console.log('nothing');
+		let protocol = req.body;
+
+		switch (req.body.protocols[0]) {
+			case 'avoid-mech':
+				return await res.status(200).send(avoidMech(protocol));
+			case 'prioritize-mech':
+				return await res.status(200).send(prioritizeMech(protocol));
+			case 'assist-allies':
+				return await res.status(200).send(assistAllies(protocol));
+			case 'avoid-crossfire':
+				return await res.status(200).send(avoidCrossfire(protocol));
+			case 'closest-enemies':
+				return await res.status(200).send(closestEnemies(protocol));
+			case 'furthest-enemies':
+				return await res.status(200).send(furthestEnemies(protocol));
 		}
 	} catch (error) {
 		res.status(500).send('Can not read the protocol you send to me');
--- server/helper_functions/functions.js
+++ server/helper_functions/functions.js
@@ -1,17 +1,86 @@
 const avoidMech = (cases) => {
-	let coord = cases.scan[0].coordinates;
-	return coord;
+	for (let i = 0; i <= cases.protocols.length; i++) {
+		if (cases.protocols[i] === 'avoid-mech') {
+			for (let j = 0; j <= cases.scan.length; j++) {
+				if (cases.scan[j].enemies.type !== 'mech') {
+					for (let k = 0; k <= cases.scan.length; k++) {
+						return cases.scan[k].coordinates;
+					}
+				}
+			}
+		}
+	}
 };
 
-const prioritizeMech = (cases) => {};
+const prioritizeMech = (cases) => {
+	for (let i = 0; i <= cases.protocols.length; i++) {
+		if (cases.protocols[i] === 'prioritize-mech') {
+			for (let j = 0; j <= cases.scan.length; j++) {
+				if (cases.scan[j].enemies.type === 'mech') {
+					for (let k = 0; k <= cases.scan.length; k++) {
+						return cases.scan[k].coordinates;
+					}
+				}
+			}
+		}
+	}
+};
 
-const closestEnemies = (cases) => {};
+const closestEnemies = (cases) => {
+	for (let i = 0; i <= cases.protocols.length; i++) {
+		if (cases.protocols[i] === 'closest-enemies') {
+			for (let j = 0; j <= cases.scan.length; j++) {}
+		}
+	}
+};
 
-const furthestEnemies = (cases) => {};
+const furthestEnemies = (cases) => {
+	for (let i = 0; i <= cases.protocols.length; i++) {
+		if (cases.protocols[i] === 'furthest-enemies') {
+			for (let j = 0; j <= cases.scan.length; j++) {
+				Math.hypot(cases.scan[j].coordinates.x, cases.scan[j].coordinates.y);
+				console.log(
+					Math.hypot(cases.scan[j].coordinates.x, cases.scan[j].coordinates.y)
+				);
+				if (
+					Math.hypot(cases.scan[j].coordinates.x, cases.scan[j].coordinates.y) >
+					Math.hypot(
+						cases.scan[j + 1].coordinates.x,
+						cases.scan[j + 1].coordinates.y
+					)
+				) {
+					return cases.scan[j].coordinates;
+				} else {
+					return cases.scan[j + 1].coordinates;
+				}
+			}
+		}
+	}
+};
 
-const assistAllies = (cases) => {};
+const assistAllies = (cases) => {
+	for (let i = 0; i <= cases.protocols.length; i++) {
+		if (cases.protocols[i] === 'assist-allies') {
+			for (let j = 0; j <= cases.scan.length; j++) {
+				if (cases.scan[j].allies) {
+					return cases.scan[j].coordinates;
+				}
+			}
+		}
+	}
+};
 
-const avoidCrossfire = (cases) => {};
+const avoidCrossfire = (cases) => {
+	for (let i = 0; i <= cases.protocols.length; i++) {
+		if (cases.protocols[i] === 'avoid-crossfire') {
+			for (let j = 0; j <= cases.scan.length; j++) {
+				if (!cases.scan[j].allies) {
+					return cases.scan[j].coordinates;
+				}
+			}
+		}
+	}
+};
 
 module.exports = {
 	avoidMech,
